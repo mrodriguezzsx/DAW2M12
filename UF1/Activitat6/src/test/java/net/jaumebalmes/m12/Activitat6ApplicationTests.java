fac4b5d2ad5b14fed1a63017ecc2d1fcdd9b50a8
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2022-11-08 15:51:17 +0100

    Inici del Projecte

---
+++ UF1/Activitat6/src/test/java/net/jaumebalmes/m12/Activitat6ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jaumebalmes.m12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activitat6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
